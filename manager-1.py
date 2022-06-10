1b680866782937c8ade55f202d804d64a0ebe1ee
Админ <Novikov_Evgeniy@mail.ru>
2022-06-10 10:43:24 +0500

    add new manager-1

--- manager-1.py
+++ manager-1.py
@@ -20,19 +20,19 @@ while True:
 
     choice = input('Выберите пункт меню: ')
     if choice == '1':
-        pass
+        from creating_folder import creation
     elif choice == '2':
-        pass
+        from creating_file import creating_fl
     elif choice == '3':
-        pass
+        from delete import delete_f
     elif choice == '4':
-        pass
+        from copying_folder import copying
     elif choice == '5':
-        pass
+        from viewing_the_directory import viewing_direct
     elif choice == '6':
-        pass
+        from  viewing_folders import viewing_fo
     elif choice == '7':
-        pass
+        from viewing_files import viewing_f
     elif choice == '8':
         print('My OS is', sys.platform)
     elif choice == '9':
@@ -50,9 +50,9 @@ while True:
                 else:
                     print('неверный пункт меню')
     elif choice == '10':
-        pass
+        from quiz_date import quiz_d
     elif choice == '11':
-        pass
+        from bank_account import actions_with_account
     elif choice == '12':
         break
     else:
